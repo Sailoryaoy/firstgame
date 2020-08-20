b7ccfda15512a04b491197e34d95d182af806e61
Sailoryaoy <69818263+Sailoryaoy@users.noreply.github.com>
2020-08-20 17:25:39 -0300

    Update and rename 0.1-nave.gd to 0.2_dual tiro, ed bad-nave.gd

--- 0.2_dual tiro, ed bad-nave.gd
+++ 0.2_dual tiro, ed bad-nave.gd
@@ -26,16 +26,25 @@ func _process(delta): # função principal do movimento
 		
 		e = 0
 		
-	set_position(get_position() + Vector2(1, 0)* vel * delta * (d + e)) #equação que determina o movimento
-	
+	set_position(get_position() + Vector2(1, 0) * vel* delta * (d + e)) #equação que determina o movimento
+	#vel nessa instacia vira um escalar, que pode ser alterado para mudar o sentido de viagem do objeto
 	# disparo
 	if Input.is_action_pressed("tiro"):
 		if ultimo_disparo <= 0:
 			var tiro = pre_tiro.instance()#variavel que representa o tiro
-			tiro.set_global_position(get_global_position())#linka a posição do tiro à da nave
+
+			tiro.set_global_position(get_node("PositionCE").get_global_position())#linka a posição do tiro à do node PositionCE
 			get_node("../").add_child(tiro)#vai buscar a main node para carregar os assets
 			ultimo_disparo = intervalo # altera o valor do ultimi dis paro pra evitar que ele dispare sem parar
+			
+			
+			tiro = pre_tiro.instance()#nessa parte n necessecita recolocar o var tiro, pois ela ja foi carregada ateriormente
+			tiro.set_global_position(get_node("PositionCD").get_global_position())#linka a posição do tiro à do node PositionCD
+			get_node("../").add_child(tiro)
+			ultimo_disparo = intervalo 
+			
 		pass
+		
 	if ultimo_disparo > 0:
 		ultimo_disparo -= delta# ultimo disparo vira menor que 0, tira o tempo do ultimo disparo
 	pass
